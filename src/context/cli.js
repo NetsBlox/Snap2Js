8145c45a6bbb26b6a020faf2b06d42e861bd269b
Brian Broll <brian.broll@vanderbilt.edu>
2017-04-28 12:54:50 -0500

    WIP #25 added cli context

---
+++ src/context/cli.js
@@ -0,0 +1,10 @@
+const base = require('./default');
+const clone = require('../utils').clone;
+
+var context = clone(base);
+
+// TODO: Add support for doAsk
+
+// TODO: Add support for key events
+
+module.exports = context;
