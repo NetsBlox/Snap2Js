501f9cf04dfe3e8fa65b42b55e7c63d9e4f0031e
Brian Broll <brian.broll@vanderbilt.edu>
2017-04-28 15:31:25 -0500

    WIP #25 Added cli context

--- src/context/cli.js
+++ src/context/cli.js
@@ -1,4 +1,4 @@
-const base = require('./default');
+const base = require('./basic');
 const clone = require('../utils').clone;
 const readline = require('readline-sync');
 
@@ -13,6 +13,35 @@ context.getLastAnswer = function(node) {
     return lastAnswer;
 };
 
-// TODO: Add support for key events
+context.__start = function(project) {
+    var stdin = process.openStdin();
+    stdin.setRawMode(true);
+    stdin.resume();
+    stdin.setEncoding('utf8');
+
+    stdin.on('data', (key) => {
+        if (key === '\u0003') process.exit();
+
+        switch (key) {
+            case '\u001b[A':
+                key = 'up arrow';
+
+            case '\u001b[B':
+                key = 'down arrow';
+
+            case '\u001b[C':
+                key = 'right arrow';
+
+            case '\u001b[C':
+                key = 'left arrow';
+
+            case ' ':
+                key = 'space';
+        }
+
+        project.sprites.concat([project.stage])
+            .forEach(obj => obj.onKeyPressed(key));
+    });
+};
 
 module.exports = context;
