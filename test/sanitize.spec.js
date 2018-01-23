b9c09fd823d2906ab3086548d314bc350ed799dd
Brian Broll <brian.broll@vanderbilt.edu>
2018-01-23 12:34:25 -0600

    WIP #66 added test for sanitizing lists

--- test/sanitize.spec.js
+++ test/sanitize.spec.js
@@ -1,6 +1,8 @@
 describe('sanitize', function() {
     const utils = require('./utils');
+    const snap2jsUtils = require('../src/utils');
     const snap2js = require('..');
+    const assert = require('assert');
 
     it('should compile with custom blocks containing `', function(done) {
         const content = utils.getContextXml('back-tick-var');
@@ -16,5 +18,12 @@ describe('sanitize', function() {
 
         const fn = snap2js.compile(content);
         fn(context);
+    });
+
+    it('should be able to sanitize arrays', function() {
+        const original = ['hey', '-world'];
+        const safeList = snap2jsUtils.sanitize(original);
+        const list = eval(safeList);
+        assert.deepEqual(list, original);
     });
 });
